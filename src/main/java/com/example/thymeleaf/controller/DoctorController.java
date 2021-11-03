a0012a77457fa13b9b24cfb2c22bbed737629035
armanmsb <arman-meshabi-ayandeh_student2021@wilder.school>
2021-11-03 16:45:27 +0100

    Final

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -1,14 +1,18 @@
 package com.example.thymeleaf.controller;
 
+import com.example.thymeleaf.model.Doctor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
-    public String doctor() {
+    @GetMapping("/doctor")
+    public String doctor(Model model, @RequestParam(value="name", defaultValue = "John Smith") String name, @RequestParam(value="number", defaultValue = "0") Integer number) {
 
-        return "doctor";
+        model.addAttribute("doctor", new Doctor(number, name));
+        return "/doctor.html";
     }
 }
--- src/main/java/com/example/thymeleaf/controller/IndexController.java
+++ src/main/java/com/example/thymeleaf/controller/IndexController.java
@@ -1,13 +1,16 @@
 package com.example.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
+        model.addAttribute("view", "doctor");
         return "index";
     }
 }
